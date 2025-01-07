1702198a8086638401ec306518302bc666f42be2
two-suns <ch13f_48@zoho.com>
2025-01-07 13:42:59 -0500

    Add fps and delta time. Add Circle Class

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    my_clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,7 @@ def main():
             
             screen.fill((0,0,0))
             pygame.display.flip()
+            dt = my_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
