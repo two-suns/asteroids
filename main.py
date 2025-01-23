ebb616dd89a0a77287177792d662bcf9f6d222ff
two-suns <ch13f_48@zoho.com>
2025-01-23 15:30:19 -0500

    Add game logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,14 +8,28 @@ def main():
     my_clock = pygame.time.Clock()
     dt = 0
     
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, radius=PLAYER_RADIUS)
+    
     while True:
+        # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return 
+        
+        dt = my_clock.tick(60) / 1000 # Calculate delta time (always after updates)
+        # Update the game state (before rendering)    
+        updatable.update(dt) # Update the player's rotation and other state
+        
+        
+        # Render the updated state to the screen    
+        screen.fill((0,0,0))
+        drawable.draw(screen)
+        pygame.display.flip()
             
-            screen.fill((0,0,0))
-            pygame.display.flip()
-            dt = my_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
